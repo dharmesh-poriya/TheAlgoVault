281cdc201cc43d39de4c9a85972b1525ab96b346
D.Poriya <dharmeshkporiya@gmail.com>
2023-01-27 11:11:14 +0530

    reverse the linked list iterative and recursive both method

---
+++ 06_LinkedList/02_Operation_SinglyLinkedList/08_reverse_linkedlist.cpp
@@ -0,0 +1,47 @@
+// https://leetcode.com/problems/reverse-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+// Iterative method
+class Solution
+{
+public:
+    ListNode *reverseList(ListNode *head)
+    {
+        ListNode *prev, *next, *current;
+        prev = next = nullptr;
+        current = head;
+        while (current)
+        {
+            next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+};
+
+// Recursive method
+class Solution
+{
+public:
+    ListNode *reverseList(ListNode *head)
+    {
+        if (!head or !head->next)
+            return head;
+
+        ListNode *newHead = reverseList(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+        return newHead;
+    }
+};
