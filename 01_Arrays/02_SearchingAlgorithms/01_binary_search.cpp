dbd2706da90b0f04bbd5078239edf5687b338edd
D.Poriya <dharmeshkporiya@gmail.com>
2023-03-06 11:17:21 +0530

    binary search for sorted array

---
+++ 01_Arrays/02_SearchingAlgorithms/01_binary_search.cpp
@@ -0,0 +1,14 @@
+int binarySearch(vector<int> &arr, int left, int right, int target)
+{
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target)
+            return mid;
+        else if (arr[mid] < target)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+    return -1;
+}
