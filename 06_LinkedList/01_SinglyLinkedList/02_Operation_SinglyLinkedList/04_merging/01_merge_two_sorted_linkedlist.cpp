72cd09da1890ecaf3b4db048ae18e9adc350c04a
D.Poriya <dharmeshkporiya@gmail.com>
2023-03-12 11:56:14 +0530

    merge two sorted linked list added

---
+++ 06_LinkedList/01_SinglyLinkedList/02_Operation_SinglyLinkedList/04_merging/01_merge_two_sorted_linkedlist.cpp
@@ -0,0 +1,43 @@
+ListNode *mergeTwoSortedLinkedList(ListNode *l1, ListNode *l2)
+{
+    if (!l1)
+        return l2;
+    if (!l2)
+        return l1;
+
+    ListNode *newHead, *current1, *current2, *tail;
+    newHead = tail = nullptr;
+    current1 = l1;
+    current2 = l2;
+    if (current1->val <= current2->val)
+    {
+        newHead = tail = current1;
+        current1 = current1->next;
+    }
+    else
+    {
+        newHead = tail = current2;
+        current2 = current2->next;
+    }
+
+    while (current1 and current2)
+    {
+        if (current1->val <= current2->val)
+        {
+            tail->next = current1;
+            tail = current1;
+            current1 = current1->next;
+        }
+        else
+        {
+            tail->next = current2;
+            tail = current2;
+            current2 = current2->next;
+        }
+    }
+    if (current1)
+        tail->next = current1;
+    else
+        tail->next = current2;
+    return newHead;
+}
